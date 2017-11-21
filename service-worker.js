1dec9cc236a593a7b5bcc2bbe5530f1bc996960b
Ajai J <ajai199@gmail.com>
2017-11-21 08:20:41 +0530

    Integrated offline support to the app

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,23 @@
-// Service Worker v1.0.0
+// Service Worker v1.0.1
 
 (function() {
   'use strict';
 
+  var CACHE_NAME = 'static-cache';
+  
+  var urlsToCache = [
+    '.',
+    'index.html',
+    'index.css'
+  ];
+
   self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
     console.log('Service worker installing...');
     self.skipWaiting();
   });
@@ -14,7 +28,32 @@
 
 
   self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+        return response || fetchAndCache(event.request);
+      })
+    );
     console.log('Fetching:', event.request.url);
   });
+
+  function fetchAndCache(url) {
+    return fetch(url)
+    .then(function(response) {
+      // Check if we received a valid response
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return caches.open(CACHE_NAME)
+      .then(function(cache) {
+        cache.put(url, response.clone());
+        return response;
+      });
+    })
+    .catch(function(error) {
+      console.log('Request failed:', error);
+      // You could return a custom offline 404 page here
+    });
+  }
 
 })();
