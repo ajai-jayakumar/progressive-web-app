f02045707c6e12f9ed045c57974e5524caa5844d
Ajai J <ajai199@gmail.com>
2017-11-21 09:18:36 +0530

    Integrated Add to Homescreen feature

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,30 @@
-// Service Worker v1.0.0
+// Service Worker v1.0.3
 
 (function() {
   'use strict';
 
   self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('static-cache-v1')
+      .then(function(cache) {
+        return cache.addAll([
+          '.',
+          'index.html',
+          'index.css',
+          'pages',
+          'http://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+          'images/still_life-1600_large_2x.jpg',
+          'images/still_life-800_large_1x.jpg',
+          'images/still_life_medium.jpg',
+          'images/still_life_small.jpg',
+          'images/horses-1600_large_2x.jpg',
+          'images/horses-800_large_1x.jpg',
+          'images/horses_medium.jpg',
+          'images/horses_small.jpg',
+          
+        ]);
+      })
+    );
     console.log('Service worker installing...');
     self.skipWaiting();
   });
@@ -14,6 +35,14 @@
 
 
   self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request)
+    .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
     console.log('Fetching:', event.request.url);
   });
 
