1d3445ba90a130af06977b835d2b9cf5d7408350
Ajai J <ajai199@gmail.com>
2017-11-23 07:46:02 +0530

    Integreated fetch api inside service worker

--- service-worker.js
+++ service-worker.js
@@ -10,11 +10,29 @@
   
   self.addEventListener('activate', function(event) {
     console.log('Service worker activating...');
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(validateResponse)
+    .catch(logError);
+
   });
 
 
   self.addEventListener('fetch', function(event) {
     console.log('Fetching:', event.request.url);
   });
+
+  function validateResponse(response) {
+
+    
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    console.log(response.json());
+  }
+
+  function logError(error) {
+    console.log('Looks like there was a problem: \n', error);
+  }
 
 })();
